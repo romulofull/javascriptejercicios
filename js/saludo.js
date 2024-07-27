cf3fdb92bf4d5c83c27ac745af524b976e88f431
romulofull <romulo.castro@gmail.com>
2024-07-27 16:52:10 -0500

    Asignacion, Console Log y Typeof

--- js/saludo.js
+++ js/saludo.js
@@ -1,7 +1,14 @@
 var nombre = "Romulo";
-var edad;
-var presenteono;
-var apellido;
+var edad = 24;
+var presenteono = true;
+var apellido = "castro";
+var estatura = 1.70;
+
+console.log("Nombre: " + nombre + typeof nombre);
+console.log("Edad: " + edad + typeof edad);  
+console.log("Status: " + presenteono + typeof presenteono);
+console.log("Apellido: " + apellido + typeof apellido);
+console.log("Estatura: " + estatura + typeof estatura);
 
 
-alert("Hola mundo con Javascript" + nombre);
+// alert("Hola mundo con Javascript" + nombre);
